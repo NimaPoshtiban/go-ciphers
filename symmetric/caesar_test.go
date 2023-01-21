31c56af2b60d55725d9b961588f99f213d36e803
Nima Poshtiban <poshtibannima@gmail.com>
2023-01-21 20:34:48 +0330

    Update caesar_test.go

--- symmetric/caesar_test.go
+++ symmetric/caesar_test.go
@@ -2,24 +2,20 @@ package symmetric
 
 import "testing"
 
-
-
 func TestEncryptCaesar(t *testing.T){
 	var exampleForEncryption Caesar = "23/AaBcbcZz12&"
 	s,_:= exampleForEncryption.Encrypt()
-	expectedResult := "23/DdEfefcC12&"
+	expectedResult := "26/DdEfefCc12&"
 	if s != expectedResult{
-		t.Errorf("Expected String(%s) is not same as"+
-		" actual string (%s)", expectedResult,s)
+		t.Errorf("Expected String(%s) is not same as actual string (%s)", expectedResult,s)
 	}
 }
 
 func TestDecryptCaesar(t *testing.T){
-	var exampleForEncryption Caesar = "23/DdEfefcC12&"
+	var exampleForEncryption Caesar = "26/DdEfefCc12&"
 	s,_:= exampleForEncryption.Decrypt()
 	expectedResult := "23/AaBcbcZz12&"
 	if s != expectedResult{
-		t.Errorf("Expected String(%s) is not same as"+
-		" actual string (%s)", expectedResult,s)
+		t.Errorf("Expected String(%s) is not same as actual string (%s)", expectedResult,s)
 	}
 }
